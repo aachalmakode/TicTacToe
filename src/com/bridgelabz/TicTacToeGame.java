95d21c74532d0416a02219a2d9fcea774dfcc673
aachalmakode <aachalmakode900@gmail.com>
2021-06-20 14:05:55 -0400

    Added : intial commit

---
+++ src/com/bridgelabz/TicTacToeGame.java
@@ -0,0 +1,15 @@
+package com.bridgelabz;
+
+import java.util.Scanner;
+
+public class TicTacToeGame {
+
+    Scanner sc=new Scanner(System.in);
+    public char[] board=new char[10];
+    public char playerChoice;
+    public char computerChoice;
+
+    public static void main(String[] args) {
+	// write your code here
+    }
+}
