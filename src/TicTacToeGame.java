e89d891a51b03e3e06d5ea784ecce4f13396dedc
aachalmakode <aachalmakode900@gmail.com>
2021-06-20 14:36:06 -0400

    player making move

--- src/TicTacToeGame.java
+++ src/TicTacToeGame.java
@@ -1,5 +1,3 @@
-package com.bridgelabz;
-
 import java.util.Scanner;
 
 public class TicTacToeGame {
@@ -43,6 +41,22 @@ public class TicTacToeGame {
         System.out.println("\n"+board[7]+" | "+board[8]+" | "+board[9]);
         System.out.println(" ");
         System.out.println(" ");
+    }
+
+    // User can make a move to a desired position
+    public void fillBoard() {
+        while(true) {
+            System.out.println("Enter the position where you want to insert: ");
+            int pos=sc.nextInt();
+            sc.nextLine();
+            if(board[pos]!=' ') {
+                System.out.println("Space not available! Select another position");
+            }
+            else {
+                board[pos]=playerChoice;
+                break;
+            }
+        }
     }
 
     public static void main(String[] args) {
